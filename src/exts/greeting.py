370da40e76ba534b4be3275f62dec6e778fad6cd
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-23 02:09:50 +0900

    挨拶

---
+++ src/exts/greeting.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+from src.bot import Bot
+
+import logging
+log = logging.getLogger(__name__)
+
+class GreetingCog(commands.Cog, name = __name__):
+  def __init__(self, bot: Bot):
+    self.bot = bot
+
+  @commands.Cog.listener()
+  async def on_message(self, msg):
+    if msg.author.bot:
+      return
+
+    match msg.content:
+      case "おはよう":
+        await msg.reply("おは")
+      case "おやすみ":
+        await msg.reply("おやすみ")
+
+async def setup(bot: Bot):
+  await bot.add_cog(GreetingCog(bot))
+
+async def teardown(bot: Bot):
+  await bot.remove_cog("GreetingCog")
+  log.info("unloaded")
