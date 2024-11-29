de53170ec21dc504ab7846de232fd84d840e96d0
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-29 12:08:20 +0900

    data

--- src/exts/anki.py
+++ src/exts/anki.py
@@ -4,9 +4,28 @@ from src.bot import Bot
 import logging
 log = logging.getLogger(__name__)
 
+DATAPATH = f'{os.path.dirname(__file__)}/../../data/anki.json'
+
 class AnkiCog(commands.Cog, name = __name__):
   def __init__(self, bot: Bot):
     self.bot = bot
+    self.data = {}
+
+  # looks not working...
+  async def cog_load(self):
+    async with aiofiles.open(DATAPATH, mode="r") as f:
+      contents = await f.read()
+    self.data = json.loads(contents)
+    self.save_data.start()
+
+  async def cog_unload(self):
+    await self.save_data()
+
+  @tasks.loop(hours=5)
+  async def save_data(self):
+    async with aiofiles.open(DATAPATH, mode="w") as f:
+      await f.write(json.dumps(self.data, indent=2, ensure_ascii=False))
+    log.info("anki.json saved")
 
 async def setup(bot: Bot):
   log.info("loaded")
