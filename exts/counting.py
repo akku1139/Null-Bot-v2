b62edc2a74684510648d80e1339201f291884284
-akku- <151517253+akku1139@users.noreply.github.com>
2025-09-16 21:33:25 +0900

    100でカウント保持がずれるのを修正

--- exts/counting.py
+++ exts/counting.py
@@ -13,7 +13,7 @@ class CountingCog(commands.Cog, name = __name__):
   async def on_reaction_add(self, reaction, user):
     if user.id == 510016054391734273: # counting bot
       match reaction.emoji:
-        case "✅":
+        case "✅" | "💯":
           self.count += 1
         case "❌":
           self.count = 0
