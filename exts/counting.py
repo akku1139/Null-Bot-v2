de7b59949599730027361698941861bb0aa4efe3
Tsuyukusa Akari (-akku-) <akkun11.open@gmail.com>
2026-01-01 00:19:34 +0900

    適当

--- exts/counting.py
+++ exts/counting.py
@@ -8,6 +8,13 @@ class CountingCog(commands.Cog, name = __name__):
   def __init__(self, bot: commands.Bot):
     self.bot = bot
     self.count: int = 0
+    self.topic_task = None
+
+  async self._do_edit(self, channel, new_topic):
+    try:
+      await channel.edit(topic=new_topic)
+    except asyncio.CancelledError:
+      pass # キャンセルされたら静かに終了
 
   @commands.Cog.listener()
   async def on_reaction_add(self, reaction, user):
@@ -18,7 +25,9 @@ class CountingCog(commands.Cog, name = __name__):
         case "❌":
           self.count = 0
 
-      await reaction.message.channel.edit(topic=f'now: {self.count} (!c now), next: {self.count+1} (!c next)')
+      if self.topic_task and not self.topic_task.done():
+        self.topic_task.cancel()
+        self.topic_task = asyncio.create_task(self._do_edit(reaction.message.channel, f'now: {self.count} (!c now), next: {self.count+1} (!c next)'))
       await asyncio.sleep(1.5)
       await reaction.message.add_reaction(reaction.emoji)
 
